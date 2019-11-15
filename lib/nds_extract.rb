f5103b558161499c77a4d8282c7ec50d3fe3b9e7
Benjamin Small <ben20262@aim.com>
2019-11-15 04:34:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,10 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  array = []
+  source.each do |direct|
+    array << direct[:name]
+  end
 end
 
 def total_gross(source)
